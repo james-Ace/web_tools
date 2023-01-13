dfdc50b13143af7c805b274d44de901750ecd656
청춘(青春) <oncwnuNDIqFuj0qEmGvTtpqEGWt0@git.weixin.qq.com>
2023-01-13 15:11:50 +0800

    创建首页

---
+++ pdf_Word.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="24">
+                <header class="toHeader">
+                    <h1>PDF转Word</h1>
+                </header>
+            </el-col>
+        </el-row>
+        <div class="main" >
+            <el-row justify="center">
+                <el-col :sm="18" :xs="10" :md="12">
+                    <p>这是一个免费的PDF转Word工具,注意:如果PDF字体太过花哨可能无法转换成可编辑状态,带公章的部分会被转换成图片</p>
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
